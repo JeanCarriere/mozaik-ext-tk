376304d1e3fe84883d55fefb1a37422cac53174e
Jean <jean.carriere@blogspirit.com>
2016-01-05 15:39:07 +0100

    add messages

--- src/components/UsersCount.jsx
+++ src/components/UsersCount.jsx
@@ -8,27 +8,26 @@ class DonutChart {
 
   constructor(bindTo, opts, data) {
     this.chart = c3.generate({
-    	bindto: bindTo,
-    	data: {
-    		columns: data,
+        bindto: bindTo,
+        data: {
+            columns: data,
             type: 'donut'
         },
-	    donut: {
-	        label: {
-	            format: function(value, ratio, id) {
-	            	console.log('format', arguments);
-	                return value + ' ' + id;
-	            },
-				show: true
-	        }
-		},
-		color: {
-		  pattern: ['#1f77b4', '#60B044']
-		},
-		legend: {
-		  hide: true
-		}
-	});
+        donut: {
+            label: {
+                format: function(value, ratio, id) {
+                    return value + ' ' + id;
+                },
+                show: true
+            }
+        },
+        color: {
+          pattern: ['#1f77b4', '#60B044']
+        },
+        legend: {
+          hide: true
+        }
+    });
   }
 
   load(data) {
@@ -36,72 +35,71 @@ class DonutChart {
   }
 
   destroy () {
-  	console.log('destroy chart');	
-  	this.chart.destroy();
+    console.log('destroy chart');   
+    this.chart.destroy();
   }
 }
 
 class UsersCount extends Component {
-	
+    
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			total: 0
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: 0
+        };
+    }
 
-	componentWillUnmount() {
-	    if (this.chart) {
-	    	this.chart.destroy();
-	    }
-  	}
+    componentWillUnmount() {
+        if (this.chart) {
+            this.chart.destroy();
+        }
+    }
 
-	getApiRequest() {
-		let { segment, title, segmentCompanies } = this.props; 
+    getApiRequest() {
+        let { segment, title, segmentCompanies } = this.props; 
 
-		return {
-			id: `intercom.getInvited.${ segment }`,
-			params: {
-				segment: segment,
-				segmentCompanies:segmentCompanies,
-				title: title
-			}
-		}
-	}
+        return {
+            id: `intercom.getInvited.${ segment }`,
+            params: {
+                segment: segment,
+                segmentCompanies:segmentCompanies,
+                title: title
+            }
+        }
+    }
 
-	onApiData(response) {
-		console.log('response', response);
-		var data = [
-			['Invited', response.invited],
+    onApiData(response) {
+        var data = [
+            ['Invited', response.invited],
             ['SignedUp', response.total]
-           	];
-		if(!this.chart) {
-			var chartElement = React.findDOMNode(this.refs.chart);
-			this.chart = new DonutChart(chartElement, null, data);
-		} else if(this.state.invited !== response.invited || this.state.total !== response.total) {
-			this.chart.load({
-				columns: data
-			});
-		}
-		this.setState({
-			invited: response.invited,
-			total: response.total,
-		});
-	}
+            ];
+        if(!this.chart) {
+            var chartElement = React.findDOMNode(this.refs.chart);
+            this.chart = new DonutChart(chartElement, null, data);
+        } else if(this.state.invited !== response.invited || this.state.total !== response.total) {
+            this.chart.load({
+                columns: data
+            });
+        }
+        this.setState({
+            invited: response.invited,
+            total: response.total,
+        });
+    }
 
-	render() {
-		return (
-			<div>
-				<div className="widget__header">
-		          Users
-		        </div>
-	        	<div className="widget__body">
-	        		<div ref="chart"></div>
-				</div>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                <div className="widget__header">
+                  Users
+                </div>
+                <div className="widget__body">
+                    <div ref="chart" className="widget__chart"></div>
+                </div>
+            </div>
+        )
+    }
 }
 
 reactMixin(UsersCount.prototype, ListenerMixin);
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import BiggestCompaniesList from './BiggestCompaniesList.jsx';
 import SegmentCompaniesCount from './SegmentCompaniesCount.jsx';
 import UsersCount from './UsersCount.jsx';
+import MessagesCount from './MessagesCount.jsx';
 
 export default {
   BiggestCompaniesList: BiggestCompaniesList,
   SegmentCompaniesCount: SegmentCompaniesCount,
-  UsersCount:UsersCount
+  UsersCount:UsersCount,
+  MessagesCount:MessagesCount
 };
