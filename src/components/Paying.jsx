3adc89fb2727f6767ca1d43c855f7ab4a4bf4b6d
Jean <jean.carriere@blogspirit.com>
2016-01-14 11:23:28 +0100

    improvements

---
+++ src/components/Paying.jsx
@@ -0,0 +1,58 @@
+import React, { Component, DOM } from 'react';
+import reactMixin from 'react-mixin';
+import { ListenerMixin } from 'reflux';
+import Mozaik from 'mozaik/browser';
+import _ from 'lodash';
+
+class Paying extends Component {
+    
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            companies: false
+        };
+    }
+
+
+    getApiRequest() {
+        let { segment, title } = this.props; 
+        return {
+            id: `intercom.getCompaniesBySegment.${ segment }`,
+            params: {
+                segment: segment,
+                title: title
+            }
+        }
+    }
+
+    onApiData(response) {
+        this.setState({
+            companies: response.segment.companies
+        });
+    }
+
+    render() {
+        var nbCompanies = this.state.companies? this.state.companies.length : 0;
+        var nbUsers = _.reduce(this.state.companies, function(memo, comp) {
+            return memo + (comp.user_count || 0);
+        }, 0);
+        return (
+            <div>
+                <div className="widget__header">
+                  Paying customers
+                </div>
+                <div className="widget__body">
+                    <div className="widget_value widget_value_small">
+                        {nbCompanies} comp. / {nbUsers} users
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+reactMixin(Paying.prototype, ListenerMixin);
+reactMixin(Paying.prototype, Mozaik.Mixin.ApiConsumer);
+
+export { Paying as default };
